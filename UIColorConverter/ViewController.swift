9d0ab297c4057dc541f59b11ae3773fee708cf1f
Ryan Lietzenmayer <RyanL@SoLos-MacBook-Pro.local>
2017-08-28 14:00:03 -0400

    Initial Commit

---
+++ UIColorConverter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UIColorConverter
+//
+//  Created by Ryan Lietzenmayer on 8/28/17.
+//  Copyright © 2017 Ryan Lietzenmayer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
