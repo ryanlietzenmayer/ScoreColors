52987b17099176aaaa2e744ca6155b8303fdac58
ryanlietzenmayer <ryan.lietz@gmail.com>
2018-03-22 15:31:35 -0400

    Change percentages of tiers.

--- UIColorConverter/SoloScoreColor.swift
+++ UIColorConverter/SoloScoreColor.swift
@@ -15,24 +15,30 @@ class SoloScoreColor: UIViewController {
     @IBOutlet weak var slider: UISlider!
     
     @IBOutlet weak var scoreLabel: UITextField!
+    @IBOutlet weak var scoreLabelPretty: UILabel!
     @IBOutlet weak var hexLabel: UILabel!
     @IBOutlet weak var redLabel: UILabel!
     @IBOutlet weak var greenLabel: UILabel!
     @IBOutlet weak var blueLabel: UILabel!
     @IBOutlet weak var percentageLabel: UILabel!
     
-    @IBOutlet weak var highTierPercentLabel: UILabel!
-    @IBOutlet weak var middleTierPercentLabel: UILabel!
-    @IBOutlet weak var lowTierPercentLabel: UILabel!
+    @IBOutlet weak var highTierPercentTextField: UITextField!
+    @IBOutlet weak var middleTierPercentTextField: UITextField!
+    @IBOutlet weak var lowTierPercentTextField: UITextField!
+    
+    @IBOutlet weak var highTierHexTextField: UITextField!
+    @IBOutlet weak var middleTierHexTextField: UITextField!
+    @IBOutlet weak var lowTierHexTextField: UITextField!
+    
     @IBOutlet weak var highTierScoreLabel: UILabel!
     @IBOutlet weak var middleTierScoreLabel: UILabel!
     @IBOutlet weak var lowTierScoreLabel: UILabel!
-    
+
     let maxScore = 99.0
     let minScore = 45.0
-    let highTierPoint = 0.75
-    let middleTierPoint = 0.50
-    let lowTierPoint = 0.25
+    var highTierPoint = 0.75
+    var middleTierPoint = 0.50
+    var lowTierPoint = 0.25
     
     //  Lightning Yellow; #FCBE24, rgb(252,190,36)
     private static let yellowLightning: UIColor = UIColor(rgb:0xFCBE24)
@@ -45,20 +51,30 @@ class SoloScoreColor: UIViewController {
     //  Burnt Orange; #F5873A, rgb(245,135,58)
     private static let orangeBurnt: UIColor = UIColor(rgb:0xF5873A)
     
+    static let goodColor: UIColor = turquoise
+    static let midColor: UIColor = greenFern
+    static let badColor: UIColor = yellowLightning
+
+    
     static let goodScoreColor: UIColor = blueKashmir
-    static let highTierPointScoreColor: UIColor = turquoise
+    var highTierPointScoreColor: UIColor = turquoise
     static let middleTierPointScoreColor: UIColor = greenFern
     static let lowTierPointScoreColor: UIColor = yellowLightning
     static let badScoreColor: UIColor = orangeBurnt
 
     override func viewDidLoad() {
         super .viewDidLoad()
-        scoreLabel.keyboardType = .numberPad
         setColor()
+
+        scoreLabel.keyboardType = .numberPad
+        let score = soloScoreFromPercent(Double(slider.value))
+        scoreLabel.text = String(format:"%.2f", score)
+        scoreLabelPretty.text = String(format:"%.0f", score)
         
-        highTierPercentLabel.text = String(format:"%.0f", highTierPoint*100) + "%"
-        middleTierPercentLabel.text = String(format:"%.0f", middleTierPoint*100) + "%"
-        lowTierPercentLabel.text = String(format:"%.0f", lowTierPoint*100) + "%"
+        highTierPercentTextField.text = String(format:"%.0f", highTierPoint*100)
+        middleTierPercentTextField.text = String(format:"%.0f", middleTierPoint*100)
+        lowTierPercentTextField.text = String(format:"%.0f", lowTierPoint*100)
+        
         highTierScoreLabel.text = String(format:"%.2f", soloScoreFromPercent(highTierPoint))
         middleTierScoreLabel.text = String(format:"%.2f", soloScoreFromPercent(middleTierPoint))
         lowTierScoreLabel.text = String(format:"%.2f", soloScoreFromPercent(lowTierPoint))
@@ -71,7 +87,9 @@ class SoloScoreColor: UIViewController {
         setColor()
         let score = soloScoreFromPercent(Double(slider.value))
         scoreLabel.text = String(format:"%.2f", score)
+        scoreLabelPretty.text = String(format:"%.0f", score)
     }
+    
     @IBAction func textChangedSoloScore(_ sender: Any) {
         if let value: Double = Double(scoreLabel.text!) {
             
@@ -81,6 +99,60 @@ class SoloScoreColor: UIViewController {
             setColor()
         }
     }
+    
+    @IBAction func highHexEditingChanged(_ sender: Any) {
+//        if let str = highTierHexTextField.text {
+//            let data = str.data(using: .utf8)!
+//            let hexString = data.map{ String(format:"%02x", $0) }.joined()
+//            let color: UIColor = UIColor(rgb:Int(hexString)!)
+//            highTierPointScoreColor = color
+////        highTierHexTextField.text = ()
+//        }
+        setColor()
+    }
+    @IBAction func midHexEditingChanged(_ sender: Any) {
+        setColor()
+    }
+    @IBAction func lowHexEditingChanged(_ sender: Any) {
+        setColor()
+    }
+    @IBAction func highPercentEditingChanged(_ sender: Any) {
+        if var value: Double = Double(highTierPercentTextField.text!) {
+            while value>1 {
+                value = value/100
+            }
+            highTierPoint = value
+            
+            let score = soloScoreFromPercent(highTierPoint)
+            highTierScoreLabel.text = String(format:"%.2f", score)
+        }
+        setColor()
+    }
+    @IBAction func midPercentEditingChanged(_ sender: Any) {
+        if var value: Double = Double(middleTierPercentTextField.text!) {
+            while value>1 {
+                value = value/100
+            }
+            middleTierPoint = value
+            
+            let score = soloScoreFromPercent(middleTierPoint)
+            middleTierScoreLabel.text = String(format:"%.2f", score)
+        }
+        setColor()
+    }
+    @IBAction func lowPercentEditingChanged(_ sender: Any) {
+        if var value: Double = Double(lowTierPercentTextField.text!) {
+            while value>1 {
+                value = value/100
+            }
+            lowTierPoint = value
+            
+            let score = soloScoreFromPercent(lowTierPoint)
+            lowTierScoreLabel.text = String(format:"%.2f", score)
+        }
+        setColor()
+    }
+    
     func soloScoreFromPercent(_ percent:Double) -> Double{
         var percentFormatted = percent
         while percentFormatted>1 {
@@ -119,12 +191,14 @@ class SoloScoreColor: UIViewController {
     }
     
     func colorFromScore(_ submittedScore: Double) -> UIColor {
+//        return colorForValueWithinRangeWithMidpointOlder(value: submittedScore, min: minScore, max: maxScore)
+
         return colorForValueWithinRangeWithMidpoint(value: submittedScore, min: minScore, max: maxScore)
     }
     func colorForValueWithinRangeWithMidpoint(value:Double, min:Double, max:Double) -> UIColor {
         let goodColor = SoloScoreColor.goodScoreColor.cgColor.components!
         let badColor = SoloScoreColor.badScoreColor.cgColor.components!
-        let middleHighColor = SoloScoreColor.highTierPointScoreColor.cgColor.components!
+        let middleHighColor = highTierPointScoreColor.cgColor.components!
         let middleColor = SoloScoreColor.middleTierPointScoreColor.cgColor.components!
         let middleLowColor = SoloScoreColor.lowTierPointScoreColor.cgColor.components!
         
@@ -159,110 +233,66 @@ class SoloScoreColor: UIViewController {
             score = score-midH
             let percentScore = score/(max-midH)
             
-            var rValue = colorValuePercentage(goodColorComponent: rGood, badColorComponent: rMidH, percentScore: percentScore)
-            var gValue = colorValuePercentage(goodColorComponent: gGood, badColorComponent: gMidH, percentScore: percentScore)
-            var bValue = colorValuePercentage(goodColorComponent: bGood, badColorComponent: bMidH, percentScore: percentScore)
+            let rValue = colorValuePercentage(goodColorComponent: rGood, badColorComponent: rMidH, percentScore: percentScore)
+            let gValue = colorValuePercentage(goodColorComponent: gGood, badColorComponent: gMidH, percentScore: percentScore)
+            let bValue = colorValuePercentage(goodColorComponent: bGood, badColorComponent: bMidH, percentScore: percentScore)
             
             let scoreColor: UIColor = UIColor(red: CGFloat(rValue/255.0),
                                               green: CGFloat(gValue/255.0),
                                               blue: CGFloat(bValue/255.0), alpha: 1.0)
             
-            redLabel.text = String(format:"%.4f", rValue)
-            greenLabel.text = String(format:"%.4f", gValue)
-            blueLabel.text = String(format:"%.4f", bValue)
-            rValue = rValue/255.0
-            gValue = gValue/255.0
-            bValue = bValue/255.0
-            hexLabel.text = String(
-                format: "%02X %02X %02X",
-                Int(rValue * 0xff),
-                Int(gValue * 0xff),
-                Int(bValue * 0xff)
-            )
+            updateTextFromRGB(red: rValue, green: gValue, blue: bValue)
             return scoreColor
         } else if value > midM{
             score = score-midM
             let percentScore = score/(midH-midM)
             
-            var rValue = colorValuePercentage(goodColorComponent: rMidH, badColorComponent: rMidM, percentScore: percentScore)
-            var gValue = colorValuePercentage(goodColorComponent: gMidH, badColorComponent: gMidM, percentScore: percentScore)
-            var bValue = colorValuePercentage(goodColorComponent: bMidH, badColorComponent: bMidM, percentScore: percentScore)
+            let rValue = colorValuePercentage(goodColorComponent: rMidH, badColorComponent: rMidM, percentScore: percentScore)
+            let gValue = colorValuePercentage(goodColorComponent: gMidH, badColorComponent: gMidM, percentScore: percentScore)
+            let bValue = colorValuePercentage(goodColorComponent: bMidH, badColorComponent: bMidM, percentScore: percentScore)
             
             let scoreColor: UIColor = UIColor(red: CGFloat(rValue/255.0),
                                               green: CGFloat(gValue/255.0),
                                               blue: CGFloat(bValue/255.0), alpha: 1.0)
             
-            redLabel.text = String(format:"%.4f", rValue)
-            greenLabel.text = String(format:"%.4f", gValue)
-            blueLabel.text = String(format:"%.4f", bValue)
-            rValue = rValue/255.0
-            gValue = gValue/255.0
-            bValue = bValue/255.0
-            hexLabel.text = String(
-                format: "%02X %02X %02X",
-                Int(rValue * 0xff),
-                Int(gValue * 0xff),
-                Int(bValue * 0xff)
-            )
+            updateTextFromRGB(red: rValue, green: gValue, blue: bValue)
             return scoreColor
             
         } else if value > midL{
             score = score-midL
             let percentScore = score/(midM-midL)
             
-            var rValue = colorValuePercentage(goodColorComponent: rMidM, badColorComponent: rMidL, percentScore: percentScore)
-            var gValue = colorValuePercentage(goodColorComponent: gMidM, badColorComponent: gMidL, percentScore: percentScore)
-            var bValue = colorValuePercentage(goodColorComponent: bMidM, badColorComponent: bMidL, percentScore: percentScore)
+            let rValue = colorValuePercentage(goodColorComponent: rMidM, badColorComponent: rMidL, percentScore: percentScore)
+            let gValue = colorValuePercentage(goodColorComponent: gMidM, badColorComponent: gMidL, percentScore: percentScore)
+            let bValue = colorValuePercentage(goodColorComponent: bMidM, badColorComponent: bMidL, percentScore: percentScore)
             
             let scoreColor: UIColor = UIColor(red: CGFloat(rValue/255.0),
                                               green: CGFloat(gValue/255.0),
                                               blue: CGFloat(bValue/255.0), alpha: 1.0)
             
-            redLabel.text = String(format:"%.4f", rValue)
-            greenLabel.text = String(format:"%.4f", gValue)
-            blueLabel.text = String(format:"%.4f", bValue)
-            rValue = rValue/255.0
-            gValue = gValue/255.0
-            bValue = bValue/255.0
-            hexLabel.text = String(
-                format: "%02X %02X %02X",
-                Int(rValue * 0xff),
-                Int(gValue * 0xff),
-                Int(bValue * 0xff)
-            )
+            updateTextFromRGB(red: rValue, green: gValue, blue: bValue)
             return scoreColor
         } else {
             score = score-min
             let percentScore = score/(midL-min)
             
-            var rValue = colorValuePercentage(goodColorComponent: rMidL, badColorComponent: rBad, percentScore: percentScore)
-            var gValue = colorValuePercentage(goodColorComponent: gMidL, badColorComponent: gBad, percentScore: percentScore)
-            var bValue = colorValuePercentage(goodColorComponent: bMidL, badColorComponent: bBad, percentScore: percentScore)
+            let rValue = colorValuePercentage(goodColorComponent: rMidL, badColorComponent: rBad, percentScore: percentScore)
+            let gValue = colorValuePercentage(goodColorComponent: gMidL, badColorComponent: gBad, percentScore: percentScore)
+            let bValue = colorValuePercentage(goodColorComponent: bMidL, badColorComponent: bBad, percentScore: percentScore)
             
             let scoreColor: UIColor = UIColor(red: CGFloat(rValue/255.0),
                                               green: CGFloat(gValue/255.0),
                                               blue: CGFloat(bValue/255.0), alpha: 1.0)
             
-            redLabel.text = String(format:"%.4f", rValue)
-            greenLabel.text = String(format:"%.4f", gValue)
-            blueLabel.text = String(format:"%.4f", bValue)
-            rValue = rValue/255.0
-            gValue = gValue/255.0
-            bValue = bValue/255.0
-            hexLabel.text = String(
-                format: "%02X %02X %02X",
-                Int(rValue * 0xff),
-                Int(gValue * 0xff),
-                Int(bValue * 0xff)
-            )
+            updateTextFromRGB(red: rValue, green: gValue, blue: bValue)
             return scoreColor
         }
     }
     //as of march 22 2018
     func colorForValueWithinRangeWithMidpointOlder(value:Double, min:Double, max:Double) -> UIColor {
-        let goodColor = SoloScoreColor.goodScoreColor.cgColor.components!
-        let badColor = SoloScoreColor.badScoreColor.cgColor.components!
-        let middleColor = SoloScoreColor.middleTierPointScoreColor.cgColor.components!
+        let goodColor = SoloScoreColor.goodColor.cgColor.components!
+        let badColor = SoloScoreColor.badColor.cgColor.components!
+        let middleColor = SoloScoreColor.midColor.cgColor.components!
         
         let rGood = Double(goodColor[0]) * 255.0
         let gGood = Double(goodColor[1]) * 255.0
@@ -287,51 +317,29 @@ class SoloScoreColor: UIViewController {
             score = score-mid
             let percentScore = score/(max-mid)
             
-            var rValue = colorValuePercentage(goodColorComponent: rGood, badColorComponent: rMid, percentScore: percentScore)
-            var gValue = colorValuePercentage(goodColorComponent: gGood, badColorComponent: gMid, percentScore: percentScore)
-            var bValue = colorValuePercentage(goodColorComponent: bGood, badColorComponent: bMid, percentScore: percentScore)
+            let rValue = colorValuePercentage(goodColorComponent: rGood, badColorComponent: rMid, percentScore: percentScore)
+            let gValue = colorValuePercentage(goodColorComponent: gGood, badColorComponent: gMid, percentScore: percentScore)
+            let bValue = colorValuePercentage(goodColorComponent: bGood, badColorComponent: bMid, percentScore: percentScore)
             
             let scoreColor: UIColor = UIColor(red: CGFloat(rValue/255.0),
                                               green: CGFloat(gValue/255.0),
                                               blue: CGFloat(bValue/255.0), alpha: 1.0)
             
-            redLabel.text = String(format:"%.4f", rValue)
-            greenLabel.text = String(format:"%.4f", gValue)
-            blueLabel.text = String(format:"%.4f", bValue)
-            rValue = rValue/255.0
-            gValue = gValue/255.0
-            bValue = bValue/255.0
-            hexLabel.text = String(
-                format: "%02X %02X %02X",
-                Int(rValue * 0xff),
-                Int(gValue * 0xff),
-                Int(bValue * 0xff)
-            )
+            updateTextFromRGB(red: rValue, green: gValue, blue: bValue)
             return scoreColor
         } else {
             score = score-min
             let percentScore = score/(mid-min)
             
-            var rValue = colorValuePercentage(goodColorComponent: rMid, badColorComponent: rBad, percentScore: percentScore)
-            var gValue = colorValuePercentage(goodColorComponent: gMid, badColorComponent: gBad, percentScore: percentScore)
-            var bValue = colorValuePercentage(goodColorComponent: bMid, badColorComponent: bBad, percentScore: percentScore)
+            let rValue = colorValuePercentage(goodColorComponent: rMid, badColorComponent: rBad, percentScore: percentScore)
+            let gValue = colorValuePercentage(goodColorComponent: gMid, badColorComponent: gBad, percentScore: percentScore)
+            let bValue = colorValuePercentage(goodColorComponent: bMid, badColorComponent: bBad, percentScore: percentScore)
             
             let scoreColor: UIColor = UIColor(red: CGFloat(rValue/255.0),
                                               green: CGFloat(gValue/255.0),
                                               blue: CGFloat(bValue/255.0), alpha: 1.0)
             
-            redLabel.text = String(format:"%.4f", rValue)
-            greenLabel.text = String(format:"%.4f", gValue)
-            blueLabel.text = String(format:"%.4f", bValue)
-            rValue = rValue/255.0
-            gValue = gValue/255.0
-            bValue = bValue/255.0
-            hexLabel.text = String(
-                format: "%02X %02X %02X",
-                Int(rValue * 0xff),
-                Int(gValue * 0xff),
-                Int(bValue * 0xff)
-            )
+            updateTextFromRGB(red: rValue, green: gValue, blue: bValue)
             return scoreColor
         }
     }
@@ -351,8 +359,23 @@ class SoloScoreColor: UIViewController {
         
     }
     
-    func updateTextFromRGB(red:Float, green:Float, blue:Float){
+    func updateTextFromRGB(red:Double, green:Double, blue:Double){
+        var rValue = red
+        var gValue = green
+        var bValue = blue
         
+        redLabel.text = String(format:"%.4f", rValue)
+        greenLabel.text = String(format:"%.4f", gValue)
+        blueLabel.text = String(format:"%.4f", bValue)
+        rValue = rValue/255.0
+        gValue = gValue/255.0
+        bValue = bValue/255.0
+        hexLabel.text = String(
+            format: "%02X %02X %02X",
+            Int(rValue * 0xff),
+            Int(gValue * 0xff),
+            Int(bValue * 0xff)
+        )
     }
     
 }
